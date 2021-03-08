289526c9773bf1698fd2d83eabee148ed9e10ae2
Emmanuel Debrah <emmanuel.debrah@turntabl.io>
2021-03-08 22:18:50 +0000

    first commit

---
+++ src/main/java/com/app/employeemanager/exception/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.app.employeemanager.exception;
+
+public class EmployeeNotFoundException extends RuntimeException{
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/app/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,15 @@
+package com.app.employeemanager.repo;
+
+import com.app.employeemanager.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+//@Repository
+public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+
+//    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
